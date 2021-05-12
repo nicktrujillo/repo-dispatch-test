b62c0eeeed752a155bcfd4ed9a97ebec4a02effe
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2021-05-11 18:01:09 -0700

    send looking glass payload in the ocotokit repo dispatch method body

--- .github/actions/local-action/main.js
+++ .github/actions/local-action/main.js
@@ -11,6 +11,17 @@ async function run() {
       owner: ctx.repo.owner,
       repo: ctx.repo.repo,
       event_type: "bread",
+      client_payload: {
+        filename: "the filename associated with the report",
+        isCorrect: true,
+        level: "info",
+        display_type: "actions",
+        msg: "the message",
+        error: {
+          expected: "the expected string",
+          got: "the gotten string",
+        },
+      },
     };
 
     const res = await octokit.repos.createDispatchEvent(requestParams);
